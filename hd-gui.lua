ccf27774bddf942df4e97bc4f3119c7fd3962881
hdgui <secondarymail.d@gmail.com>
2025-01-02 04:02:38 +0000

    Update hd-gui.lua

--- hd-gui.lua
+++ hd-gui.lua
@@ -11,7 +11,7 @@ local Window = Rayfield:CreateWindow({
 	Name = "HD-GUI",
 	Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
 	LoadingTitle = "HD-GUI",
-	LoadingSubtitle = "DIN-2",
+	LoadingSubtitle = "DIN-22",
 	Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
 	DisableRayfieldPrompts = false,
@@ -92,6 +92,7 @@ local InfiniteJumpToggle = MovementTab:CreateToggle({
 	Callback = function(Value)
 		-- The function that takes place when the toggle is pressed
 		-- The variable (Value) is a boolean on whether the toggle is true or false
+			local InfiniteJumpEnabled
 		if Value then
 			InfiniteJumpEnabled = true
 		elseif not Value then
